af22c2f5303e2c3737bcd00fd2c742c6aa2f34d4
Madhusri <madhusridamarasingu@gmail.com>
2026-09-22 21:51:13 +0530

    Add backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Configure Node.js to use Google's DNS servers to resolve MongoDB SRV records
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
